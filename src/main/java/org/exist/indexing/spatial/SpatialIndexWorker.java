70656d626e3956a7a5e222914f4fb715b37ff77e
Pierrick Brihaye <pierrick.brihaye@free.fr>
2026-03-20 23:29:39 +0100

    Update SpatialIndexWorker.java

--- src/main/java/org/exist/indexing/spatial/SpatialIndexWorker.java
+++ src/main/java/org/exist/indexing/spatial/SpatialIndexWorker.java
@@ -5,17 +5,13 @@ import org.exist.dom.persistent.NodeProxy;
 import org.exist.indexing.AbstractIndexWorker;
 import org.exist.indexing.IndexController;
 import org.exist.storage.txn.Txn;
-import org.exist.xquery.index.Occurrences; // Changement d'adresse pour Occurrences
+import org.exist.xquery.Occurrences; // Retour à l'import standard
 
-/**
- * Worker spatial mis à jour pour eXist-db 6.2.0
- */
 public class SpatialIndexWorker extends AbstractIndexWorker {
 
     private final SpatialIndex index;
 
     public SpatialIndexWorker(SpatialIndex index, IndexController controller) {
-        // Dans eXist 6, le constructeur attend l'index et le contrôleur
         super(index, controller);
         this.index = index;
     }
@@ -27,10 +23,9 @@ public class SpatialIndexWorker extends AbstractIndexWorker {
 
     @Override
     public void occurrence(Txn transaction, NodeProxy node, Occurrences occurrences) {
-        // Extraction de la géométrie GML
-        String gml = node.getStringValue();
-        if (gml != null && (gml.contains("gml:") || gml.contains("<Geometry"))) {
-            index.getStore().addGeometry(transaction, node, gml);
+        String value = node.getStringValue();
+        if (value != null && value.contains("gml:")) {
+            index.getStore().addGeometry(transaction, node, value);
         }
     }
 
