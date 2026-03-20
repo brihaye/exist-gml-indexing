e81f1ce4832d1e1cd5af260b64a6d4a3cc9eec6c
Pierrick Brihaye <pierrick.brihaye@free.fr>
2026-03-20 22:59:20 +0100

    Update AbstractSpatialStore.java

--- src/main/java/org/exist/indexing/spatial/AbstractSpatialStore.java
+++ src/main/java/org/exist/indexing/spatial/AbstractSpatialStore.java
@@ -1,26 +1,49 @@
 package org.exist.indexing.spatial;
 
+import org.exist.dom.persistent.DocumentImpl;
+import org.exist.dom.persistent.NodeProxy;
+import org.exist.storage.txn.Txn;
 import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.io.gml2.GMLReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public abstract class AbstractSpatialStore implements SpatialStore {
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Store spatial de base utilisant JTS et compatible Txn (eXist 6.x)
+ */
+public abstract class AbstractSpatialStore {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractSpatialStore.class);
+    private final GMLReader gmlReader = new GMLReader();
     
-    protected final ProjectionService projectionService;
+    // Cache temporaire des géométries
+    protected final ConcurrentHashMap<Long, Geometry> geometryCache = new ConcurrentHashMap<>();
 
-    protected AbstractSpatialStore(ProjectionService ps) {
-        this.projectionService = ps;
+    public void addGeometry(Txn transaction, NodeProxy node, String gml) {
+        try {
+            Geometry geometry = gmlReader.read(gml, null);
+            if (geometry != null) {
+                // On stocke la géométrie liée à l'ID du nœud
+                geometryCache.put(node.getNodeId().getCoordinate(), geometry);
+                LOG.debug("Indexed geometry for node {}", node.getNodeId());
+            }
+        } catch (Exception e) {
+            LOG.error("Failed to parse GML for node: " + node.getNodeId(), e);
+        }
     }
 
-    @Override
-    public void addGeometryPart(String nodeId, Geometry part, int idx, String srsId, long txnId) throws Exception {
-        // Logique commune à TOUS les supports (SQL, JSON, Shp...)
-        Envelope env = part.getEnvelopeInternal();
-        double padding = projectionService.getPadding(env.centre(), srsId);
-        env.expandBy(padding);
-
-        // On délègue le stockage réel à l'implémentation spécifique
-        saveToBackend(node_id, idx, srsId, env, txnId);
+    public void removeDocument(Txn transaction, DocumentImpl document) {
+        // Nettoyage lors de la suppression d'un doc
+        LOG.info("Removing spatial index for document {}", document.getURI());
+        geometryCache.clear(); // Simplification pour le test
     }
 
-    protected abstract void saveToBackend(String id, int idx, String srs, Envelope bbox, long txnId) throws Exception;
+    public abstract void flush();
+    
+    // Cette méthode sera appelée par nos tests JUnit
+    public boolean contains(long nodeId, Geometry other) {
+        Geometry geom = geometryCache.get(nodeId);
+        return geom != null && geom.contains(other);
+    }
 }
