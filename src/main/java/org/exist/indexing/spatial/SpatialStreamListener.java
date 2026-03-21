413037a797b45d8dfb39cf90dd3e71c0358fefac
Pierrick Brihaye <pierrick.brihaye@free.fr>
2026-03-21 20:30:33 +0100

    Update SpatialStreamListener.java

--- src/main/java/org/exist/indexing/spatial/SpatialStreamListener.java
+++ src/main/java/org/exist/indexing/spatial/SpatialStreamListener.java
@@ -1,15 +1,18 @@
 package org.exist.indexing.spatial;
 
-import org.exist.indexing.StreamListener;
 import org.exist.dom.persistent.DocumentImpl;
-import org.exist.dom.persistent.ElementImpl;
+import org.exist.dom.persistent.NodeImpl;
+import org.exist.indexing.StreamListener;
+import org.exist.storage.NodePath;
 import org.exist.storage.txn.Txn;
-import org.exist.util.NodePath;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.io.gml2.GMLReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SpatialStreamListener implements StreamListener {
+    private static final Logger LOG = LogManager.getLogger(SpatialStreamListener.class);
     private final SpatialStorageEngine storage;
     private final DocumentImpl doc;
     private StringBuilder buffer = new StringBuilder();
@@ -21,39 +24,45 @@ public class SpatialStreamListener implements StreamListener {
     }
 
     @Override
-    public void startElement(org.exist.storage.txn.Txn transaction, org.exist.dom.persistent.ElementImpl element, org.exist.util.NodePath path) {
+    public void startElement(Txn transaction, org.exist.dom.persistent.ElementImpl element, NodePath path) {
         String srs = element.getAttribute("srsName");
         if (srs != null) this.currentSrs = srs;
         
         if (element.getNamespaceURI().equals("http://www.opengis.net/gml")) {
-            buffer.setLength(0); // On commence à capturer les coordonnées
+            buffer.setLength(0);
         }
     }
 
     @Override
-    public void endElement(org.exist.storage.txn.Txn transaction, org.exist.dom.persistent.ElementImpl element, org.exist.util.NodePath path) {
+    public void endElement(Txn transaction, org.exist.dom.persistent.ElementImpl element, NodePath path) {
         if (element.getNamespaceURI().equals("http://www.opengis.net/gml") && buffer.length() > 0) {
             try {
                 Geometry geom = new GMLReader().read(buffer.toString(), null);
-                String nodeId = element.getNodeId().toString();
-                String parentId = element.getParentNode().getNodeId().toString();
+                // Cast obligatoire en NodeImpl pour accéder à getNodeId()
+                String nodeId = ((NodeImpl)element).getNodeId().toString();
+                String parentId = ((NodeImpl)element.getParentNode()).getNodeId().toString();
 
-                // On indexe la géométrie principale
                 storage.indexGeometry(doc, nodeId, parentId, geom, currentSrs, "GEOMETRY");
 
-                // GESTION DES TROUS (Donuts)
                 if (geom instanceof Polygon poly) {
                     for (int i = 0; i < poly.getNumInteriorRing(); i++) {
-                        storage.indexGeometry(doc, nodeId + "_hole_" + i, nodeId, 
-                            poly.getInteriorRingN(i), currentSrs, "INTERIOR_RING");
+                        storage.indexGeometry(doc, nodeId + "_h" + i, nodeId, 
+                            poly.getInteriorRingN(i), currentSrs, "HOLE");
                     }
                 }
-            } catch (Exception e) { /* Log error */ }
+            } catch (Exception e) {
+                LOG.error("GML Parsing error", e);
+            }
         }
     }
 
     @Override
-    public void characters(org.exist.storage.txn.Txn transaction, char[] ch, int start, int len) {
+    public void characters(Txn transaction, char[] ch, int start, int len) {
         buffer.append(ch, start, len);
+    }
+
+    @Override
+    public void endIndexDocument(Txn transaction) {
+        // Méthode obligatoire en eXist 6
     }
 }
