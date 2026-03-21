89049365fe0064223786a748a0cda8aca01f9957
Pierrick Brihaye <pierrick.brihaye@free.fr>
2026-03-21 13:48:19 +0100

    Update SpatialIndexWorker.java

--- src/main/java/org/exist/indexing/spatial/SpatialIndexWorker.java
+++ src/main/java/org/exist/indexing/spatial/SpatialIndexWorker.java
@@ -22,7 +22,7 @@ public class SpatialIndexWorker implements IndexWorker {
     private final SpatialIndex index;
     private final DBBroker broker;
     private DocumentImpl doc;
-    private org.exist.indexing.StreamListener.ReindexMode mode;
+    private StreamListener.ReindexMode mode;
 
     public SpatialIndexWorker(SpatialIndex index, DBBroker broker) {
         this.index = index;
@@ -34,27 +34,33 @@ public class SpatialIndexWorker implements IndexWorker {
         return "http://exist-db.org/indexing/spatial";
     }
 
-    // ON UTILISE LE NOM COMPLET POUR FORCER LE COMPILATEUR
+    // 1. LA MÉTHODE MODERNE (eXist 6)
     @Override
-    public void setDocument(org.exist.dom.persistent.DocumentImpl doc, org.exist.indexing.StreamListener.ReindexMode mode) {
+    public void setDocument(DocumentImpl doc, StreamListener.ReindexMode mode) {
         this.doc = doc;
         this.mode = mode;
     }
 
+    // 2. LA MÉTHODE DÉPRÉCIÉE (Celle qui cause l'erreur "not abstract")
+    // On l'implémente pour satisfaire le contrat de l'interface
     @Override
-    public org.exist.indexing.StreamListener.ReindexMode getMode() {
-        return mode;
+    public void setDocument(DocumentImpl doc) {
+        this.setDocument(doc, StreamListener.ReindexMode.STREAMS);
     }
 
     @Override
-    public org.exist.dom.persistent.DocumentImpl getDocument() {
+    public void setMode(StreamListener.ReindexMode mode) {
+        this.mode = mode;
+    }
+
+    @Override
+    public DocumentImpl getDocument() {
         return doc;
     }
 
-    // eXist 6.2.0 demande souvent setMode séparément aussi
     @Override
-    public void setMode(org.exist.indexing.StreamListener.ReindexMode mode) {
-        this.mode = mode;
+    public StreamListener.ReindexMode getMode() {
+        return mode;
     }
 
     @Override
@@ -99,7 +105,8 @@ public class SpatialIndexWorker implements IndexWorker {
         }
     }
 
-    @Override
+    // On retire l'annotation @Override ici, car si elle n'est plus dans l'interface,
+    // c'est elle qui fait échouer la compilation à la ligne 102.
     public void remove(Txn transaction, DocumentImpl document) {
         if (index.getStore() != null && document != null) {
             index.getStore().removeDocument(transaction, document);
