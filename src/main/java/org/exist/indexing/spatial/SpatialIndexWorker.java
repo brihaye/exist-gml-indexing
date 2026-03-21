2458f6a03d597986af43c412d8757d506aeeb0c1
Pierrick Brihaye <pierrick.brihaye@free.fr>
2026-03-21 20:31:31 +0100

    Update SpatialIndexWorker.java

--- src/main/java/org/exist/indexing/spatial/SpatialIndexWorker.java
+++ src/main/java/org/exist/indexing/spatial/SpatialIndexWorker.java
@@ -1,38 +1,38 @@
 package org.exist.indexing.spatial;
 
-import org.exist.indexing.AbstractIndexWorker; // C'EST LUI QUI MANQUE !
+import org.exist.indexing.AbstractIndexWorker;
 import org.exist.indexing.Index;
 import org.exist.indexing.StreamListener;
 import org.exist.storage.DBBroker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SpatialIndexWorker extends AbstractIndexWorker {
+    private static final Logger LOG = LogManager.getLogger(SpatialIndexWorker.class);
     private final SpatialStorageEngine storage;
 
-    public SpatialIndexWorker(SpatialIndex index, SpatialStorageEngine storage, DBBroker broker) {
+    public SpatialIndexWorker(Index index, SpatialStorageEngine storage, DBBroker broker) {
         super(index, broker);
         this.storage = storage;
     }
 
     @Override
     public StreamListener getStreamListener() {
-        // Chaque indexation de document reçoit un nouveau Listener
         return new SpatialStreamListener(storage, getDocument());
     }
 
     @Override
     public void removeDocument() {
         try {
-            // NETTOYAGE : Invoqué par eXist lors d'une suppression de document
             storage.deleteDocument(getDocument().getDocId());
+            LOG.info("Spatial data removed for doc: " + getDocument().getDocId());
         } catch (Exception e) {
-            LOG.error("Sync error: SQL cleanup failed", e);
+            LOG.error("Cleanup failed", e);
         }
     }
 
     @Override
-    public boolean checkIndex(DBBroker broker) {
-        // Vérification de cohérence (Garbage Collection)
-        // [Implémentation de parcours des DOC_ID orphelins comme discuté]
-        return true; 
+    public void updateDocument() {
+        removeDocument();
     }
 }
