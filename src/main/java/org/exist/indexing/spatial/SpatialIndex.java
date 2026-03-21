c267263d31a476127b7014840fef4dc30c467cae
Pierrick Brihaye <pierrick.brihaye@free.fr>
2026-03-21 08:54:49 +0100

    Update SpatialIndex.java

--- src/main/java/org/exist/indexing/spatial/SpatialIndex.java
+++ src/main/java/org/exist/indexing/spatial/SpatialIndex.java
@@ -2,12 +2,10 @@ package org.exist.indexing.spatial;
 
 import org.exist.indexing.AbstractIndex;
 import org.exist.indexing.IndexWorker;
-import org.exist.indexing.IndexController;
 import org.exist.storage.DBBroker;
 import org.exist.storage.BrokerPool;
 import java.nio.file.Path;
 import org.w3c.dom.Element;
-import java.util.Map;
 
 public class SpatialIndex extends AbstractIndex {
 
@@ -15,20 +13,19 @@ public class SpatialIndex extends AbstractIndex {
 
     @Override
     public void configure(BrokerPool pool, Path path, Element config) {
-        // Signature eXist 6 respectée
         ProjectionService projectionService = new ProjectionService();
         this.store = new BBoxOrientedSQLStore(projectionService);
     }
 
     @Override
     public boolean checkIndex(DBBroker broker) {
-        // Doit renvoyer boolean pour eXist 6
         return true; 
     }
 
+    // LA SIGNATURE QUE MAVEN RÉCLAME : DBBroker au lieu de IndexController
     @Override
-    public IndexWorker getWorker(IndexController controller) {
-        return new SpatialIndexWorker(this, controller);
+    public IndexWorker getWorker(DBBroker broker) {
+        return new SpatialIndexWorker(this, broker);
     }
 
     public AbstractSpatialStore getStore() {
